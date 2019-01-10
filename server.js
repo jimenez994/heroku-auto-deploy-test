a446b12ecb107b29582324ac35ad9e6a3371e8fa
JJimenez <j.jimenez.994@hotmail.com>
2019-01-10 15:38:58 -0500

    workibng?

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = 8000; 
+const port = 8002; 
 const app = express();
 
-app.use(bodyParser).urlencoded({ extended:  false });
+// app.use(bodyParser).urlencoded({ extended:  false });
 app.use(bodyParser.json());
 app.use(express.json());
 // app.use()
+
+// app.get('/', (req, res) => {
+//     res.send({message: "great it works?"});
+// })
 
 require("./server/config/mongoose");
 require("./server/config/routes")(app);
 
 app.listen(port, () => {
     console.log(`Hey you are in port ${port}`);
-})
+});
 
 
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,14 +1,5 @@
-const mongoose = require('mongoose');
-const fs = require('fs');
-
-// mongoose.connect("mongodb://zeus:test12345@ds247852.mlab.com:47852/daycare", { useNewUrlParser: true });
-mongoose.connect("mongodb://localhost/auto_deploy_testDB", { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
-
-var models_path = __dirname + "/../models";
-fs.readdirSync(models_path).forEach((file) => {
-  if(file.includes('.js')){
-    console.log("loading" + file + "...");
-    require(models_path + "/" + file);
-  }
-})
+module.exports = (app) => {
+    app.get('/', (req, res) => {
+        res.send({message: "great it works?"});
+    })
+}
