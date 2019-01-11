7ab287aa5446bdf71dd70f3481c7140f82d4d95f
JJimenez <j.jimenez.994@hotmail.com>
2019-01-11 09:29:45 -0500

    -

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,20 @@
+
+const path = require("path")
+
 module.exports = (app) => {
     app.get('/', (req, res) => {
         res.send({message: "great!, is it working?"});
     })
+
+    // Server static assets if in production
+    if(process.env.NODE_ENV === 'production'){
+        // Set static folder
+        app.use(express.static('client/build'));
+
+        // Any route that gets hit will go to the index.html 
+        app.get('*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+        })
+
+    }
 }
