ebd975b679026dd2f33322c23b6d99dd158449aa
JJimenez <j.jimenez.994@hotmail.com>
2019-01-13 17:52:22 -0500

    _

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import WeatherApp from './componets/weatherApp';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Adding react to the project is a success!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Adding react to the project is a success!
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+              Learn React
+            </a>
+            <Link to="/weather"> Weather</Link>
+          <Route path="/weather" component={WeatherApp}/>
+          </header>
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/componets/weatherApp.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class weatherApp extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Weather app</h2>
+      </div>
+    )
+  }
+}
