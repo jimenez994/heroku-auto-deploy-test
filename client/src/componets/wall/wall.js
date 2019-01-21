fa8550de761d621859823634b157532ef79d27b9
JJimenez <j.jimenez.994@hotmail.com>
2019-01-21 13:55:21 -0500

    _

--- client/src/componets/wall/wall.js
+++ client/src/componets/wall/wall.js
@@ -4,7 +4,7 @@ import Axios from 'axios';
 export default class Wall extends Component { 
 
     componentDidMount(){
-        Axios.get(`http://localhost:5000/api/post/all`)
+        Axios.get(`/api/post/all`)
             .then(posts => console.log(posts.data))
             .catch(err => console.log(err)
             )
